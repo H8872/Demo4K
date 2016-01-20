4af6eb592812c5b6708d0a5cab24f9687bbd2893
H8872 <H8872@student.jamk.fi>
2016-01-20 14:59:35 +0200

    Added VolumeSlide class.

---
+++ T5/VolumeSlide.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class VolumeSlide
+    {
+        private readonly int maxVolume = 9;
+        private readonly int minVolume = 0;
+
+        private int volume;
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume)
+                {
+                    if (value >= minVolume) volume = value;
+                    else
+                    {
+                        Console.WriteLine("Volume too low - volume set to min");
+                        volume = minVolume;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Volume too high - volume set to max");
+                    volume = maxVolume;
+                }
+            }
+        }
+
+    }
+}
